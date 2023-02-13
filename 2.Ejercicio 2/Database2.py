fe89abbacb5b2ec555a6810a1f6c1a1bc6449b5f
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-13 18:22:20 +0100

    ahiuuu

--- 2.Ejercicio 2/Database2.py
+++ 2.Ejercicio 2/Database2.py
@@ -7,8 +7,27 @@ Multiplica el numero_usuario por 9 en sí mismo
 Multiplica el numero_magico por el numero_usuario en sí mismo
 Finalmente muestra el valor final del numero_magico por pantalla
 """
-numero_magico = 12345679
-def operacion_num_mag():
-    numero_usuario = input("introduzca un numero entre el 1 y el 9:")
-    secondnum = numero_usuario*9
-    return numero_magico*secondnum
+#necesitamos proporcionar un programa que dado un numero entre el 1 y el 9 haga dichas operaciones ademas forzaremos a que el programa
+#obligue introducir dicho numero con esos parametros
+#Empecemos por el numero_usuario
+class numero_final:
+    def __init__(self, numero_usuario):
+        self.numero_usuario = numero_usuario
+        while True:
+            numero_usuario = input("introduzca un numero entre el 1 y el 9:")
+            try:
+                numero_usuario = int(numero_usuario)
+            except:
+                break
+            if 0 < numero_usuario < 10:
+                break
+            else:
+                pass
+    def operacion(self):
+        numero_magic = 12345679
+        nuevo_num = self.numero_usuario*9
+        numero_magico = nuevo_num * numero_magic
+        return numero_magico
+print(numero_final.operacion(self=None))
+
+
