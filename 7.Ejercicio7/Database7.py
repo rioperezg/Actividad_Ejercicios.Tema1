4b97f316ade1f23f64078ec49ebb6b6a3e7c1700
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-20 18:13:29 +0100

    i want too

---
+++ 7.Ejercicio7/Database7.py
@@ -0,0 +1,10 @@
+"""
+Realiza una función llamada agregar_una_vez(lista, el) que reciba una lista y un elemento. La función debe añadir el 
+elemento al final de la lista con la condición de no repetir ningún elemento. Además si este elemento ya se encuentra 
+en la lista se debe invocar un error de tipo ValueError que debes capturar y mostrar este mensaje en su lugar:
+Error: Imposible añadir elementos duplicados => [elemento].
+Cuando tengas la función intenta añadir los siguiente valores a la lista 10, -2, "Hola" y luego muestra su contenido.
+Sugerencia
+Puedes utilizar la sintaxis "elemento in lista"
+elementos = [1, 5, -2]
+"""
