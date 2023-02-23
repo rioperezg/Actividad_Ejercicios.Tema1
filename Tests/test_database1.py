7e755d8db2785a8b6d2a319b9606b8c84057ccb1
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-23 11:25:47 +0100

    ahora zi

--- Tests/test_database1.py
+++ Tests/test_database1.py
@@ -4,3 +4,12 @@ import copy
 # import helpers
 import unittest
 import Database1 as db
+
+class TestDatabase(unittest.TestCase):
+    def setUp(self):
+        db.nombre_y_apellidos.__init__ = "zereP nauJ, 01"
+    def test_cadena(self):
+        cadena_modificada = db.nombre_y_apellidos.Cadena("Juan Perez ha sacado un 10")
+        cadena_no_modificada = db.nombre_y_apellidos.Cadena("zereP nauJ, 01")
+        self.assertIsNotNone(cadena_modificada)
+        self.assertIsNone(cadena_no_modificada)
