214c0f859fab34172c766392ca29f3580eec6a30
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-23 13:56:57 +0100

    luego vemos

--- Tests/test_database6.py
+++ Tests/test_database6.py
@@ -4,3 +4,11 @@ import copy
 # import helpers
 import unittest
 import Database6 as db
+class TestDatabase5(unittest.TestCase):
+    def setUp6(self):
+        db.Separar = "Estos son los numeros pares:" [42, 56, 18, 76], "y estos son los numeros impares:" [23, 35, 81]
+    def test_Separar(self): 
+        Lista_separada = db.Separar("Estos son los numeros pares:" [42, 56, 18, 76], "y estos son los numeros impares:" [23, 35, 81])
+        Lista_no_separada = db.Separar([23,42,56,18,35,76,81])
+        self.assertEqual(Lista_separada)
+        self.assertNotEqual(Lista_no_separada)
--- Tests/test_database7.py
+++ Tests/test_database7.py
@@ -4,3 +4,8 @@ import copy
 # import helpers
 import unittest
 import Database7 as db
+class TestDatabase5(unittest.TestCase):
+    def setUp6(self):
+        db.agregar_una_vez = 
+                        pass
+                print("Elemento:{}, in lista".format(el))
