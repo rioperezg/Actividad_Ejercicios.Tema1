cd7f725b39351bc6e7c8014db0f6f9550ff02b62
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-23 13:26:07 +0100

    oueyaç

--- Tests/test_database5.py
+++ Tests/test_database5.py
@@ -4,3 +4,22 @@ import copy
 # import helpers
 import unittest
 import Database5 as db
+class TestDatabase3(unittest.TestCase):
+    def setUp5(self):
+        db.Descomposicion = [000008,
+                             000070,
+                             000600,
+                             003000,
+                             020000,
+                             100000]
+    def test_Descomposicion(self):
+        numero_descompuesto = db.Descomposicion([000008,
+                                                 000070,
+                                                 000600,
+                                                 003000,
+                                                 020000,
+                                                 100000])
+        numero_sin_descomponer = db.Descomposicion(123678)
+        self.assertEqual(numero_descompuesto)
+        self.assertNotEqual(numero_sin_descomponer)                                         
+                         
