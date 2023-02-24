070aca30232dcb3195d9d5fc501cc825b7cb113e
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-24 13:01:02 +0100

    oleee

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-# import Database1 as db1
+import Database1 as db1
 # import Database2 as db2
 # import Database3 as db3
 # import Database4 as db4
@@ -6,9 +6,24 @@
 # import Database6 as db6
 # import Database7 as db7
 def iniciar():
-    opcion = input("Introduzca el numero del ejercicio que desea ejecutar:")
+    opcion = int(input("Introduzca el numero del ejercicio que desea ejecutar:"))
     if opcion == 1:
         print("Ha seleccionado el ejercicio 1")
-        # print(db1.nombre_y_apellidos.Cadena)
+        P = db1.nombre_y_apellidos(input("Introduzca la cadena que desea voltear:"))
+        print(db1.nombre_y_apellidos.Cadena(self=P))
+    elif opcion == 2:
+        print("Ha seleccionado el ejercicio 2")
+    elif opcion == 3:
+        print("Ha seleccionado el ejercicio 3")
+    elif opcion == 4:
+        print("Ha seleccionado el ejercicio 4")
+    elif opcion == 5:
+        print("Ha seleccionado el ejercicio 5")
+    elif opcion == 6:
+        print("Ha seleccionado el ejercicio 6")
+    elif opcion == 7:
+        print("Ha seleccionado el ejercicio 7")
+    else:
+        print("Solo se encuentran 7 ejercicios disponibles")                   
 
 print(iniciar())
