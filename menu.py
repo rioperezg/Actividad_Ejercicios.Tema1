46f0d1dd8bd376b63f2d40d0e2da3068c2836dd1
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-24 11:29:50 +0100

    outea

---
+++ menu.py
@@ -0,0 +1,14 @@
+# import Database1 as db1
+# import Database2 as db2
+# import Database3 as db3
+# import Database4 as db4
+# import Database5 as db5
+# import Database6 as db6
+# import Database7 as db7
+def iniciar():
+    opcion = input("Introduzca el numero del ejercicio que desea ejecutar:")
+    if opcion == 1:
+        print("Ha seleccionado el ejercicio 1")
+        # print(db1.nombre_y_apellidos.Cadena)
+
+print(iniciar())
