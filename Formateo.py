f1d6f15860e682b1c12d206c2cca862a59262482
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-27 18:51:51 +0100

    Prueba funcion formateo

--- Formateo.py
+++ Formateo.py
@@ -7,8 +7,7 @@ def leerLista_nums(strr):
     lista_nums =[]
     strr = strr.split(",")
     for i in strr:
-        if i == 2 or 3:
            i = int(i)
            lista_nums.append(i)
     return lista_nums
-print(leerLista_nums("2,3"))    
+# print(leerLista_nums("2,3,hola"))    
