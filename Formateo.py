6edb4859e5e5404b024681e9db8c20ff3176cb25
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-27 12:10:06 +0100

    veamos

--- Formateo.py
+++ Formateo.py
@@ -3,3 +3,11 @@ def leerLista_pantalla(stri):
     return cadena
 # print(leerLista_pantalla("2Hacer la comida,3limpiar la casa,1hacer la cama"))
 # print(leerLista_pantalla("2,3,4,56,76"))
+def leerLista_nums(strr):
+    lista_nums =[]
+    strr = strr.split(",")
+    for i in strr:
+        i = int(i)
+        lista_nums.append(i)
+    return lista_nums
+# print(leerLista_nums("2,3"))    
