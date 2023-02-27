0b1dd6ed9629eedc56d8cfb4d4e4ba3ed105e212
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-27 22:18:02 +0100

    completado testdatabase2

--- Tests/test_database2.py
+++ Tests/test_database2.py
@@ -5,4 +5,11 @@ import copy
 import unittest
 import Database2 as db2
 class TestDatabase2(unittest.TestCase):
+    def setUp2(self):
+        db2.numero_final.__init__= 4
+    def test_operacion(self):
+        numero_obtenido = db2.numero_final.operacion(444444444)
+        numero_noObtenido = db2.numero_final.operacion(4)
+        self.assertEqual(numero_obtenido)
+        self.assertNotEqual(numero_noObtenido)
     
