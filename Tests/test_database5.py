4a4903c989f2ea57e997c58cea28f3af960e6c6a
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-02-27 19:47:02 +0100

    modificado testdatabase5

--- Tests/test_database5.py
+++ Tests/test_database5.py
@@ -6,19 +6,19 @@ import unittest
 import Database5 as db5
 class TestDatabase5(unittest.TestCase):
     def setUp5(self):
-        db5.Descomposicion = [000008,
-                             000070,
-                             000600,
-                             003000,
-                             020000,
-                             100000]
+        db5.Descomposicion = ("000008")
+        ("000070")
+        ("000600")
+        ("003000")
+        ("020000")
+        ("100000")
     def test_Descomposicion(self):
-        numero_descompuesto = db5.Descomposicion([000008,
-                                                 000070,
-                                                 000600,
-                                                 003000,
-                                                 020000,
-                                                 100000])
+        numero_descompuesto = db5.Descomposicion(("000008")
+                             ("000070")
+                             ("000600")
+                             ("003000")
+                             ("020000")
+                             ("100000"))
         numero_sin_descomponer = db5.Descomposicion(123678)
         self.assertEqual(numero_descompuesto)
         self.assertNotEqual(numero_sin_descomponer)                                         
